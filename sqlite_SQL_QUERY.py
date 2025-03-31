198db1fe545311303d827a75bfe7d2a07d1b7619
Dismont <shin195802@gmail.com>
2025-03-31 18:24:07 +0300

    docs (sqlite_SQL_QUERY.py): Закомментировал строки

--- sqlite_SQL_QUERY.py
+++ sqlite_SQL_QUERY.py
@@ -343,8 +343,8 @@ def main():
     # db.insert_into_cwe(data="SQL QUERY/INSERT_cwe_parentof_query_fix.sql")
 
 # insert `CWE_to_CVE`
-    db.bruter_file(data="SQL QUERY/INSERT_cwe_to_cve_query.sql")
-    db.insert_into_cwe(data="SQL QUERY/INSERT_cwe_to_cve_query_fix.sql")
+    # db.bruter_file(data="SQL QUERY/INSERT_cwe_to_cve_query.sql")
+    # db.insert_into_cwe(data="SQL QUERY/INSERT_cwe_to_cve_query_fix.sql")
 
 
 # --- --- --- --- --- DROPPER --- --- --- --- --- --- ---
