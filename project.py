1fce94309ad8659bb1e859ecbfaf6a05fa88956c
Dismont <shin195802@gmail.com>
2025-04-08 12:58:06 +0300

    feat (project): Отредактировал основной файл

--- project.py
+++ project.py
@@ -2,9 +2,16 @@ import entity
 
 def main():
     PC1 = entity.PersonalComputer()
-    PC1.print_all_data()
+    PC1.configuration.update({"Hostname": "PC1"})
+
 
     PC2 = entity.PersonalComputer()
+    PC2.configuration.update({"Hostname": "PC2"})
+    PC1.network_layer.update({"RJ-45": PC1})
+
+    PC1.print_all_data()
+    PC2.print_all_data()
+
 
 
 if __name__ == "__main__":
