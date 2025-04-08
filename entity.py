c249a9d47af9f5cb3c2e035d4d1b97ed79eafeba
Dismont <shin195802@gmail.com>
2025-04-08 09:44:40 +0300

    feat (project): Создан новый файл для инициализации ПК

---
+++ entity.py
@@ -0,0 +1,58 @@
+import json
+class PersonalComputer:
+
+    def __init__(self):
+
+        self.network_layer = {
+                "Type" : "PC"
+        }
+        self.hardware_layer = {
+            "MotherBoard" : "",
+            "CPU" : "",
+            "RAM" : "",
+            "GPU" : "",
+            "Storage" : "",
+            "Power unit" : ""
+        }
+        self.system_layer = {
+            "OS" : "",
+            "OS version" : ""
+        }
+
+        self.application_layer = {
+            "App": "",
+            "App version": ""
+        }
+
+        self.user_layer = {
+            "Mouse" : "",
+            "Keyboard" : "",
+            "Display" : ""
+        }
+
+
+
+    def get_network_cve(self):
+        pass
+
+    def get_hardware_cve(self):
+        pass
+
+    def get_system_cve(self):
+        pass
+
+    def get_app_cve(self):
+        pass
+
+    def get_user_cve(self):
+        pass
+
+    def print_all_data(self):
+        print(f"""
+        Network Layer       : {self.network_layer},
+        Hardware Layer      : {self.hardware_layer},
+        System Layer        : {self.system_layer},
+        Application Layer   : {self.application_layer},
+        User Layer          : {self.user_layer}
+        """)
+
