d7bd3d8aece6dd9f87aa6116837187157ec413e2
Dismont <shin195802@gmail.com>
2025-04-11 11:44:17 +0300

    feat (project): Создал класс `Database` для работы с тест-БД

--- PROJECT/entity.py
+++ PROJECT/entity.py
@@ -181,4 +181,95 @@ class Router:
             if value == "": continue
             else: query.append(value)
 
-        return  query
+        return query
+
+
+class Database:
+
+    def __init__(self):
+        # --- CREATE query ---
+        self.create_capec \
+            = """create table IF NOT EXISTS `CAPEC` (
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                capec_id INTEGER NOT NULL UNIQUE,
+                capec_name VARCHAR(150) NOT NULL UNIQUE,
+                capec_description TEXT NOT NULL,
+                capec_type VARCHAR(50) NOT NULL);"""
+        self.create_cwe \
+            = """create table IF NOT EXISTS `CWE` (
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                cwe_id INTEGER NOT NULL UNIQUE,
+                cwe_name VARCHAR(150) NOT NULL UNIQUE,
+                cwe_description TEXT NOT NULL,
+                cwe_type VARCHAR(50) NOT NULL);"""
+        self.create_cve \
+            = """create table IF NOT EXISTS `CVE` (
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                cve_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, 
+                cve_name VARCHAR(150) NOT NULL UNIQUE,
+                cve_description TEXT NOT NULL,
+                cve_link VARCHAR(150) NOT NULL UNIQUE,
+                cve_cvss_v2_vector VARCHAR(150) NOT NULL,
+                cve_cvss_v2_basescore INTEGER NOT NULL,
+                cve_cvss_v3_vector VARCHAR(150) NOT NULL,
+                cve_cvss_v3_basescore INTEGER NOT NULL );"""
+        self.create_cpe \
+            = """create table IF NOT EXISTS `CPE` (
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                cve_id INTEGER NOT NULL UNIQUE, 
+                cpe_string VARCHAR(60) NOT NULL UNIQUE,
+                cpe_status VARCHAR(30) NOT NULL, 
+                cpe_link VARCHAR(60) NOT NULL UNIQUE, 
+                cpe_part VARCHAR(40) NOT NULL, 
+                cpe_vendor VARCHAR(40) NOT NULL, 
+                cpe_product VARCHAR(40) NOT NULL, 
+                cpe_version VARCHAR(20) NOT NULL, 
+                cpe_update VARCHAR(30) NOT NULL, 
+                cpe_edition VARCHAR(30) NOT NULL, 
+                cpe_language VARCHAR(30) NOT NULL, 
+                cpe_edition_sw VARCHAR(30) NOT NULL, 
+                cpe_target_sw VARCHAR(30) NOT NULL, 
+                cpe_target_hw VARCHAR(30) NOT NULL, 
+                cpe_other VARCHAR(30) NOT NULL);"""
+
+        # --- CREATE *CROSS-TABLE* query ---
+        self.create_capec_to_cwe \
+            = """create table IF NOT EXISTS `CAPEC_to_CWE` (
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                capec_id INTEGER NOT NULL,
+                cwe_id INTEGER NOT NULL);"""
+        self.create_capec_parentof \
+            = """create table IF NOT EXISTS `CAPEC_parentof` (
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                capec_parent INTEGER NOT NULL,
+                capec_child  INTEGER NOT NULL);"""
+        self.create_cwe_to_cve \
+            = """create table IF NOT EXISTS `CWE_to_CVE` (
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                cwe_id INTEGER NOT NULL,
+                cve_id INTEGER NOT NULL);"""
+        self.create_cwe_parentof \
+            = """create table IF NOT EXISTS `CWE_parentof` (
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                cwe_parent INTEGER NOT NULL,
+                cwe_child  INTEGER NOT NULL);"""
+
+        # --- INSERT query ---
+        self.insert_capec \
+            = "insert into `CAPEC` (capec_id, capec_name, capec_description, capec_type) values"
+        self.insert_cwe \
+            = "insert into `CWE` (cwe_id, cwe_name, cwe_description, cwe_type) values"
+        self.insert_cve \
+            = "insert into `CVE` (cve_id, cve_name, cve_description, cve_link, cve_cvss_v2_vector, cve_cvss_v2_basescore, cve_cvss_v3_vector, cve_cvss_v3_basescore) values"
+        self.insert_cpe \
+            = "insert into `CPE` (cve_id, cpe_string, cpe_status, cpe_link, cpe_part, cpe_vendor, cpe_product, cpe_version, cpe_update, cpe_edition, cpe_language, cpe_edition_sw, cpe_target_sw, cpe_target_hw, cpe_other) values"
+
+        # --- INSERT *CROSS-TABLE* query ---
+        self.insert_capec_parentof \
+            = "insert into `CAPEC_parentof` (capec_parent, capec_child) values"
+        self.insert_capec_to_cwe \
+            = "insert into `CAPEC_to_CWE` (capec_id, cwe_id) values"
+        self.insert_cwe_parentof \
+            = "insert into `CWE_parentof` (cwe_parent, cwe_child) values"
+        self.insert_cwe_to_cve \
+            = "insert into `CWE_to_CVE` (cwe_id, cve_id) values"
